93e33d0cb99464ee6027e621521e4b2ae5e64a63
imane-benattou <imane.benattou@etu.uae.ac.ma>
2023-05-24 15:02:24 +0100

    add registerPresident

--- api/routes/api.php
+++ api/routes/api.php
@@ -17,13 +17,15 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::group(['middleware' => ['auth:sanctum']], function()
 {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/registerPresident', [AuthController::class, 'registerPresident'])->name('registerPresident')->middleware('IsAdminUniv');
     Route::post('/registerAdmin', [AuthController::class, 'registerAdmin'])->name('registerAdmin')->middleware('IsAdminUniv');
     Route::post('/registerEns', [AuthController::class, 'registerEnseignant'])->name('registerEnseignant')->middleware('IsAdminEtab');
 });
 
-Route::get('/show/{PPR}', [AdminController::class, 'show'])->name('Admin.show');
-Route::get('/show/{email}', [UserController::class, 'show'])->name('User.show');
-Route::put('/update/{PPR}', [AdminController::class, 'update'])->name('Admin.update');
-Route::put('/update/{email}', [UserController::class, 'update'])->name('User.update');
-Route::delete('/destroy/{PPR}', [AdminController::class, 'destroy'])->name('Admin.destroy');
-Route::delete('/destroy/{email}', [UserController::class, 'destroy'])->name('User.destroy');
+
+Route::get('/showAdmin/{PPR}', [AdminController::class, 'show'])->name('Admin.show');
+Route::get('/showUser/{email}', [UserController::class, 'show'])->name('User.show');
+Route::put('/updateAdmin/{PPR}', [AdminController::class, 'update'])->name('Admin.update');
+Route::put('/updateUser/{email}', [UserController::class, 'update'])->name('User.update');
+Route::delete('/destroyAdmin/{PPR}', [AdminController::class, 'destroy'])->name('Admin.destroy');
+Route::delete('/destroyUser/{email}', [UserController::class, 'destroy'])->name('User.destroy');
